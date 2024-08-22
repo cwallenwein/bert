c4d20deaa7468c4ecacece5ea7bd19db3f05ad92
cwallenwein <chris.wallenwein@gmail.com>
2024-08-22 11:02:18 +0200

    make warmup stable decay lr schedule more verbose to spot error

--- trainer/scheduler/warmup_stable_decay_scheduler.py
+++ trainer/scheduler/warmup_stable_decay_scheduler.py
@@ -71,10 +71,15 @@ class DynamicWarmupStableDecayScheduler(LRScheduler):
         super().__init__(optimizer, last_epoch, verbose)
 
     def start_decay(self, current_steps: int, training_progress_pct: float = 0.8):
+        print("Starting decay of learning rate")
         self.decaying = True
         decay_pct = 1 - training_progress_pct
-        self.total_steps = int(current_steps / training_progress_pct)
-        self.decay_steps = int(self.total_steps * decay_pct)
+        total_steps = int(current_steps / training_progress_pct)
+        assert total_steps > 0, f"current_steps: {current_steps}, training_progress_pct: {training_progress_pct}"
+        decay_steps = int(total_steps * decay_pct)
+        assert decay_steps > 0, f"total_steps: {total_steps}, decay_pct: {decay_pct}"
+        self.decay_steps = decay_steps
+        self.total_steps = total_steps
 
     def get_lr(self) -> List[float]:
         step = self.last_epoch
@@ -86,10 +91,10 @@ class DynamicWarmupStableDecayScheduler(LRScheduler):
         if step < self.warmup_steps:
             # warmup
             return self.lr * self.linear_warmup_function(step=step)
-        if self.warmup_steps <= step and not self.decaying:
+        elif not self.decaying:
             # constant
             return self.lr
-        elif self.decaying:
+        else:
             # decay
             return self.lr * self.sqrt_decay_function(step=step)
 
