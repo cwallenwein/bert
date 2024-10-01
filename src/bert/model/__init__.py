2cb0ed480da1499d597d604d1a1dcce548156fa1
cwallenwein <chris.wallenwein@gmail.com>
2024-10-01 16:16:40 +0200

    change folder structure

---
+++ src/bert/model/__init__.py
@@ -0,0 +1,15 @@
+from .bert import (
+    BertConfig,
+    BertModel,
+    BertModelForMLM,
+    BertModelForMLMandandNSP,
+    BertModelForSequenceClassification,
+)
+
+__all__ = [
+    "BertConfig",
+    "BertModel",
+    "BertModelForMLM",
+    "BertModelForMLMandandNSP",
+    "BertModelForSequenceClassification",
+]
